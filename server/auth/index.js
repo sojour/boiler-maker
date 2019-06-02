8842b94ff500dd4b532c884c6b94d5f2efe73ed2
Wei Ji <monokuroai@gmail.com>
2019-06-02 15:00:48 -0400

    added up to login without oauth

---
+++ server/auth/index.js
@@ -0,0 +1,55 @@
+const router = require('express').Router()
+const User = require('../db/models/User')
+module.exports = router
+
+
+router.put('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    });
+    if (!user) {
+      res.status(401).send('User not found');
+    } else if (!user.hasMatchingPassword(req.body.password)) {
+      res.status(401).send('Incorrect password');
+    } else {
+      req.login(user, err => {
+        if (err) next(err);
+        else res.json(user);
+      });
+    }
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    req.login(newUser, err => {
+      if (err) next(err);
+      else res.json(newUser);
+    });
+
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists');
+    } else {
+      next(err)
+    }
+  }
+});
+
+
+router.delete('/logout', (req, res, next) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+})
+
+
+router.get('/me', (req, res, next) => {
+  res.json(req.user);
+})
--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./db/models/User')
 
 const app = express();
+//passport registration
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then(user => done(null, user))
+    .catch(done);
+});
+
+
 
 app.use(morgan('dev'));
 
@@ -11,6 +30,17 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'configure this',
+  resave: false,
+  // this option says if I am new but not modified still save
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', require('./auth'))
 app.use('/api', require('./apiRoutes'));
 
 
