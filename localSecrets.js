14be8094e3d868430ed0e62c5f6a8b95761356f6
Wei Ji <43001677+sojour@users.noreply.github.com>
2019-06-02 20:20:10 -0400

    Deleted localSecrets.js

--- localSecrets.js
+++
@@ -1,3 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = '1043656613791-dp20ol3h8rbmsgv5hoaoka3mfohe722n.apps.googleusercontent.com';
-process.env.GOOGLE_CLIENT_SECRET = 'c6Rg5746A0cK8C_VY40SRkXH'
-process.env.GOOGLE_CALLBACK = 'http://localhost:3000/auth/google/callback'
